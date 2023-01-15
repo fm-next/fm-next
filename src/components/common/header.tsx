c60fe4de5c372afc4959fbe8a60782efbb90119e
birnamwood <birnamwood19@gmail.com>
2023-01-15 18:37:24 +0900

    トップページからドキュメントへのリンクを削除

---
+++ src/components/common/header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg relative flex w-full items-center justify-between bg-white py-2 shadow-md">
+        <span>FM-NEXT</span>
+      </nav>
+    </header>
+  )
+}
